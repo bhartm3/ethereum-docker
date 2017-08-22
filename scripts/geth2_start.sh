eeafa25eee187b56b4d1e1b6a7e403f38fe5a6b6
bhartm3 <bhartm3@gmail.com>
2017-08-22 18:36:47 +0000

    fixed typo

--- scripts/geth2_start.sh
+++ scripts/geth2_start.sh
@@ -10,7 +10,7 @@ geth --datadir "$datadir" --networkid 271093 --port "30302" --maxpeers "3" --ide
 #geth --datadir "$datadir" --networkid 271093 --bootnodes "$bootnode_enode" --port "30302" --maxpeers "3" --identity "geth2" console 2>> /root/.ethereum/geth2/console.log
 
 echo 1234 > /password.txt
-geth account new --datadir "/root/.ethereum/geth1/" --password /password.txt
+geth account new --datadir "/root/.ethereum/geth2/" --password /password.txt
 
 while :
 do
