826b40b1dfdf964c057ae7f439538ec51f6968ec
bhartm3 <bhartm3@gmail.com>
2017-08-22 19:21:13 +0000

    manual script changes

--- scripts/geth1_manual.sh
+++ scripts/geth1_manual.sh
@@ -6,4 +6,22 @@ bootnode_ip=`cat /root/.ethereum/bootnode/bootnode_ip.txt`
 bootnode_enode="$bootnode_enode_id@$bootnode_ip:30301"
 echo $bootnode_enode
 
-geth --datadir "$datadir" --networkid 271093 --bootnodes "$bootnode_enode" --port "30302" --maxpeers "3" --identity "geth1" console 2>> /root/.ethereum/geth1/console.log
+case "$1" in
+
+ basic)
+	geth --datadir "$datadir" --networkid 271093 --bootnodes "$bootnode_enode" --port "30302" --maxpeers "3" --identity "geth1" console 2>> /root/.ethereum/geth1/console.log
+	;;
+
+ mine)
+	geth --datadir "$datadir" --networkid 271093 --bootnodes "$bootnode_enode" --port "30302" --maxpeers "3" --identity "geth1" --mine --minerthreads=1 --etherbase=0x0000000000000000000000000000000000000000 console 2>> /root/.ethereum/geth1/console.log
+	;;
+
+ custom)
+	echo "enter custom options:"
+	read options
+        geth --datadir "$datadir" --networkid 271093 --bootnodes "$bootnode_enode" --port "30302" --maxpeers "3" --identity "geth1" $options console 2>> /root/.ethereum/geth1/console.log
+	;;
+   *)
+	echo "no option specified: basic|mine|custom"
+	exit 0 
+esac
